2b9baa6a8fdaff7ccc0fc06f9055f7f9c4b00b65
darshiny07 <133087658+darshiny07@users.noreply.github.com>
2024-05-29 17:18:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ import './index.css';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
-
-
 ReactDOM.render(
   <React.StrictMode>
- <Provider store={store}>
+    <Provider store = {store}>
       <App />
       </Provider>
   </React.StrictMode>,
