23901118fc8fb068567830ffb572858d21628fbf
darshiny07 <133087658+darshiny07@users.noreply.github.com>
2024-05-29 17:17:26 +0530

    Add files via upload

--- counterSlice.js
+++ counterSlice.js
@@ -1,28 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice  } from "@reduxjs/toolkit";
 
-const initialState = {
-    count: 0
+const initialState ={
+    count : 0
 }
 
 export const counterSlice = createSlice({
-    name: 'counter',
+    name : "counter",
     initialState,
-    reducers: {
-        increment: (state) => {
-            state.count += 1;
+    reducers : {
+        increment : (state)=>{
+            state.count +=1;
         },
-        decrement: (state) => {
-            state.count -= 1;
-        },
-        reset: (state) => {
-            state.count = 0;
-        },
-        incrementByAmount: (state, action) => {
-            state.count += action.payload;
+        decrement : (state)=>{
+            state.count -=1;
         }
     }
-});
+})
 
-export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
+export const {increment , decrement} = counterSlice.actions;
 
 export default counterSlice.reducer;
